991d80d9ad90e117e286459cd90bb764711764bf
renaaaaan <renanxc444@gmail.com>
2026-07-14 10:30:10 -0300

    Initialize Discord bot client with login

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 require('dotenv').config();
 
-console.log('🚀 Setup Bot iniciando...');
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once('ready', () => {
+  console.log(`✅ ${client.user.tag} online!`);
+});
+
+client.login(process.env.TOKEN);
